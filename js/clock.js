21ce004ad692af6a3006b3914a4159bece862858
larilarila <larilarila@naver.com>
2019-12-14 17:02:52 +0900

    clock.js
    & add seconds

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const timeBox = document.querySelector(".clock"),
+  clockIn = timeBox.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockIn.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
